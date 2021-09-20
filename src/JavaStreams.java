b96d7385cd8db44ff0572f90a103714af70248c8
Sajan <sajanmhatre29@gmail.com>
2021-09-20 15:19:26 +0530

    Use Stream Map Function

--- src/JavaStreams.java
+++ src/JavaStreams.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 public class JavaStreams {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Streams...");
@@ -7,9 +9,15 @@ public class JavaStreams {
         List<Integer> myNumberList =  new ArrayList<Integer>();
         for (int i =0; i<5; i++)
             myNumberList.add(i);
+        //Method to Store Double Value
+        Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
         //Method 1: Processing the Stream
-        myNumberList.stream().forEach(n ->{
-            System.out.println("Method_1: Stream forEach Value: "+n);
-        });
+//        myNumberList.stream().forEach(n ->{
+//            System.out.println("Method_1: Stream forEach Value: "+n);
+//        });
+        //Method 2: Process the Stream, Apply Operations on the Stream and then
+        //Store the Result
+        List<Double> streamList = myNumberList.stream().map(toDoubleFunction).collect(Collectors.toList());
+        System.out.println("Method_2: Printing Double List: "+ streamList);
     }
 }
