5aa7144b41e939bc87039f73e71923494f6d38bb
Sajan <sajanmhatre29@gmail.com>
2021-09-20 15:11:59 +0530

    Created a Stream

--- src/JavaStreams.java
+++ src/JavaStreams.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 public class JavaStreams {
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Streams");
+        System.out.println("Welcome to Java Streams...");
+        //Creating Sample Collection
+        List<Integer> myNumberList =  new ArrayList<Integer>();
+        for (int i =0; i<5; i++)
+            myNumberList.add(i);
+        //Method 1: Processing the Stream
+        myNumberList.stream().forEach(n ->{
+            System.out.println("Method_1: Stream forEach Value: "+n);
+        });
     }
 }
