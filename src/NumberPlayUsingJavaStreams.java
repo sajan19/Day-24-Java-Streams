cf8e60c94b0676e7fa382db1e3bbfd48db36d59c
Sajan <sajanmhatre29@gmail.com>
2021-09-23 01:27:19 +0530

    Number Play Elements in Ascending Order

--- src/NumberPlayUsingJavaStreams.java
+++ src/NumberPlayUsingJavaStreams.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-public class JavaStreams {
+public class NumberPlayUsingJavaStreams {
     public static void main(String[] args) {
         System.out.println("Welcome to Java Streams...");
         //Creating Sample Collection
